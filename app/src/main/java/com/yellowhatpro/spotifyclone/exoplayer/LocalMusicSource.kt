9317f32b3e1d1b38ebbf12467127592fbd5f4a60
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-06-09 07:42:38 +0530

    setup localmusicsource: very much clear now

--- app/src/main/java/com/yellowhatpro/spotifyclone/exoplayer/LocalMusicSource.kt
+++ app/src/main/java/com/yellowhatpro/spotifyclone/exoplayer/LocalMusicSource.kt
@@ -1,8 +1,63 @@
 package com.yellowhatpro.spotifyclone.exoplayer
 
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaBrowserCompat.MediaItem.FLAG_PLAYABLE
+import android.support.v4.media.MediaDescriptionCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.MediaMetadataCompat.*
+import androidx.core.net.toUri
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.yellowhatpro.spotifyclone.data.local.SongRepository
 import com.yellowhatpro.spotifyclone.exoplayer.State.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class LocalMusicSource {
+class LocalMusicSource @Inject constructor(
+    private val musicRepository: SongRepository
+) {
+
+    var songs = emptyList<MediaMetadataCompat>()
+
+    suspend fun fetchMediaData() = withContext(Dispatchers.IO) {
+        state = STATE_INITIALIZING
+        val listOfAllSongs = musicRepository.fetchSongs()
+        songs = listOfAllSongs.map { song->
+            Builder()
+                .putString(METADATA_KEY_ARTIST, song.artist)
+                .putString(METADATA_KEY_MEDIA_ID, song.mediaID)
+                .putString(METADATA_KEY_TITLE, song.title)
+                .putString(METADATA_KEY_MEDIA_URI, song.uri.toString())
+                .build()
+        }
+        state = STATE_INITIALIZED
+    }
+
+    fun asMediaSource(dataSourceFactory: DefaultDataSourceFactory) : ConcatenatingMediaSource{
+        val concatenatingMediaSource = ConcatenatingMediaSource()
+        songs.forEach{ song ->
+            val mediaItem = MediaItem.fromUri(song.getString(METADATA_KEY_MEDIA_URI))
+            val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(mediaItem)
+            concatenatingMediaSource.addMediaSource(mediaSource)
+        }
+        return concatenatingMediaSource
+    }
+
+    fun asMediaItem() = songs.map { song ->
+        val mediaUri = song.getString(METADATA_KEY_MEDIA_URI) ?: "null"
+        val desc = MediaDescriptionCompat.Builder()
+            .setMediaUri(mediaUri.toUri())
+            .setTitle(song.description.title)
+            .setMediaId(song.description.mediaId)
+            .setSubtitle(song.description.subtitle)
+            .build()
+        MediaBrowserCompat.MediaItem(desc, FLAG_PLAYABLE)
+
+    }
 
     private val onReadyListeners = mutableListOf<(Boolean) -> Unit>()
 
